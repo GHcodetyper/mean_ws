611d168a59afa7617f2cf7bd49b0a309f829b6f3
ghcodetyper <codetyper@gmail.com>
2017-11-28 04:28:59 +0200

    20171128_2

--- user/index.js
+++ user/index.js
@@ -1,12 +1,17 @@
 //exports
 //global
+//var
+
+//the order of seeking for modules: .js, .node, .json
+
+var phrases = require('./ru');
 
 function User(name) {
     this.name = name;
 }
 
 User.prototype.hello = function (who) {
-    console.log("Hello, " + who.name);
+    console.log(phrases.Hello + ', ' + who.name);
 }
 
 console.log("user.js is required");
